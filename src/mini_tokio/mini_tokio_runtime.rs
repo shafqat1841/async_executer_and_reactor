4d927be5c3f18c6c3ff4142ffae349e29dd0f075
shafqat1841 <shafqat1841@gmail.com>
2026-05-27 17:40:38 +0500

    chore: refactor code

--- src/mini_tokio/mini_tokio_runtime.rs
+++ src/mini_tokio/mini_tokio_runtime.rs
@@ -1,69 +1,49 @@
 use std::future::Future;
-use std::sync::{Arc, mpsc};
+use std::sync::{Arc, Mutex, mpsc};
 
-use crate::mini_tokio::constants::CURRENT;
-use crate::mini_tokio::task::Task;
+use futures::future::BoxFuture;
+use futures::task::{self, ArcWake};
+use std::task::Context;
 
-/// A very basic futures executor based on a channel. When tasks are woken, they
-/// are scheduled by queuing them in the send half of the channel. The executor
-/// waits on the receive half and executes received tasks.
-///
-/// When a task is executed, the send half of the channel is passed along via
-/// the task's Waker.
+pub struct Task {
+    pub future: Mutex<BoxFuture<'static, ()>>,
+    pub executor: mpsc::Sender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let _ = arc_self.executor.send(arc_self.clone());
+    }
+}
+
 pub struct MiniTokio {
-    // Receives scheduled tasks. When a task is scheduled, the associated future
-    // is ready to make progress. This usually happens when a resource the task
-    // uses becomes ready to perform an operation. For example, a socket
-    // received data and a `read` call will succeed.
     scheduled: mpsc::Receiver<Arc<Task>>,
-
-    // Send half of the scheduled channel.
     sender: mpsc::Sender<Arc<Task>>,
 }
 
 impl MiniTokio {
-    /// Initialize a new mini-tokio instance.
     pub fn new() -> MiniTokio {
         let (sender, scheduled) = mpsc::channel();
 
         MiniTokio { scheduled, sender }
     }
-
-    /// Spawn a future onto the mini-tokio instance.
-    ///
-    /// The given future is wrapped with the `Task` harness and pushed into the
-    /// `scheduled` queue. The future will be executed when `run` is called.
     pub fn spawn<F>(&self, future: F)
     where
         F: Future<Output = ()> + Send + 'static,
     {
-        Task::spawn(future, &self.sender);
-    }
-
-    /// Run the executor.
-    ///
-    /// This starts the executor loop and runs it indefinitely. No shutdown
-    /// mechanism has been implemented.
-    ///
-    /// Tasks are popped from the `scheduled` channel receiver. Receiving a task
-    /// on the channel signifies the task is ready to be executed. This happens
-    /// when the task is first created and when its waker has been used.
-    pub fn run(&self) {
-        // Set the CURRENT thread-local to point to the current executor.
-        //
-        // Tokio uses a thread-local variable to implement `tokio::spawn`. When
-        // entering the runtime, the executor stores necessary context with the
-        // thread-local to support spawning new tasks.
-        CURRENT.with(|cell| {
-            *cell.borrow_mut() = Some(self.sender.clone());
+        let task = Arc::new(Task {
+            future: Mutex::new(Box::pin(future)),
+            executor: self.sender.clone(),
         });
 
-        // The executor loop. Scheduled tasks are received. If the channel is
-        // empty, the thread blocks until a task is received.
+        let _ = self.sender.send(task);
+    }
+    pub fn run(&self) {
         while let Ok(task) = self.scheduled.recv() {
-            // Execute the task until it either completes or cannot make further
-            // progress and returns `Poll::Pending`.
-            task.poll();
+            let waker = task::waker(task.clone());
+            let mut cx = Context::from_waker(&waker);
+            let mut future = task.future.try_lock().unwrap();
+            let _ = future.as_mut().poll(&mut cx);
         }
     }
 }
--- src/mini_tokio/process_data.rs
+++ src/mini_tokio/process_data.rs
@@ -1,36 +1,12 @@
-
 use std::pin::Pin;
-use std::sync::{ Arc, Mutex};
+use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, Waker};
 use std::thread;
 use std::time::{Duration, Instant};
 
-// Asynchronous equivalent to `thread::sleep`. Awaiting on this function pauses
-// for the given duration.
-//
-// mini-tokio implements delays by spawning a timer thread that sleeps for the
-// requested duration and notifies the caller once the delay completes. A thread
-// is spawned **per** call to `delay`. This is obviously a terrible
-// implementation strategy and nobody should use this in production. Tokio does
-// not use this strategy. However, it can be implemented with few lines of code,
-// so here we are.
 pub async fn delay(dur: Duration) {
-    
-    // `delay` is a leaf future. Sometimes, this is referred to as a "resource".
-    // Other resources include sockets and channels. Resources may not be
-    // implemented in terms of `async/await` as they must integrate with some
-    // operating system detail. Because of this, we must manually implement the
-    // `Future`.
-    //
-    // However, it is nice to expose the API as an `async fn`. A useful idiom is
-    // to manually define a private future and then use it from a public `async
-    // fn` API.
     struct Delay {
-        // When to complete the delay.
         when: Instant,
-        // The waker to notify once the delay has completed. The waker must be
-        // accessible by both the timer thread and the future so it is wrapped
-        // with `Arc<Mutex<_>>`
         waker: Option<Arc<Mutex<Waker>>>,
     }
 
@@ -38,17 +14,8 @@ pub async fn delay(dur: Duration) {
         type Output = ();
 
         fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
-            // First, if this is the first time the future is called, spawn the
-            // timer thread. If the timer thread is already running, ensure the
-            // stored `Waker` matches the current task's waker.
             if let Some(waker) = &self.waker {
                 let mut waker = waker.lock().unwrap();
-
-                // Check if the stored waker matches the current tasks waker.
-                // This is necessary as the `Delay` future instance may move to
-                // a different task between calls to `poll`. If this happens, the
-                // waker contained by the given `Context` will differ and we
-                // must update our stored waker to reflect this change.
                 if !waker.will_wake(cx.waker()) {
                     *waker = cx.waker().clone();
                 }
@@ -56,53 +23,26 @@ pub async fn delay(dur: Duration) {
                 let when = self.when;
                 let waker = Arc::new(Mutex::new(cx.waker().clone()));
                 self.waker = Some(waker.clone());
-
-                // This is the first time `poll` is called, spawn the timer thread.
                 thread::spawn(move || {
                     let now = Instant::now();
 
                     if now < when {
                         thread::sleep(when - now);
                     }
-
-                    // The duration has elapsed. Notify the caller by invoking
-                    // the waker.
                     let waker = waker.lock().unwrap();
                     waker.wake_by_ref();
                 });
             }
-
-            // Once the waker is stored and the timer thread is started, it is
-            // time to check if the delay has completed. This is done by
-            // checking the current instant. If the duration has elapsed, then
-            // the future has completed and `Poll::Ready` is returned.
             if Instant::now() >= self.when {
                 Poll::Ready(())
             } else {
-                // The duration has not elapsed, the future has not completed so
-                // return `Poll::Pending`.
-                //
-                // The `Future` trait contract requires that when `Pending` is
-                // returned, the future ensures that the given waker is signaled
-                // once the future should be polled again. In our case, by
-                // returning `Pending` here, we are promising that we will
-                // invoke the given waker included in the `Context` argument
-                // once the requested duration has elapsed. We ensure this by
-                // spawning the timer thread above.
-                //
-                // If we forget to invoke the waker, the task will hang
-                // indefinitely.
                 Poll::Pending
             }
         }
     }
-
-    // Create an instance of our `Delay` future.
     let future = Delay {
         when: Instant::now() + dur,
         waker: None,
     };
-
-    // Wait for the duration to complete.
     future.await;
 }
