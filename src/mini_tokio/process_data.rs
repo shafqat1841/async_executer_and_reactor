d61055f9bc59f41af313a05177c14ac64136be3d
shafqat1841 <shafqat1841@gmail.com>
2026-05-27 18:31:58 +0500

    chore: refactor code

--- src/mini_tokio/process_data.rs
+++ src/mini_tokio/process_data.rs
@@ -1,48 +1,52 @@
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
-use std::task::{Context, Poll, Waker};
+use std::task::{Context, Poll};
 use std::thread;
 use std::time::{Duration, Instant};
 
 pub async fn delay(dur: Duration) {
     struct Delay {
         when: Instant,
-        waker: Option<Arc<Mutex<Waker>>>,
+
+        has_waker: bool,
     }
 
     impl Future for Delay {
         type Output = ();
 
         fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
-            if let Some(waker) = &self.waker {
-                let mut waker = waker.lock().unwrap();
-                if !waker.will_wake(cx.waker()) {
-                    *waker = cx.waker().clone();
-                }
-            } else {
-                let when = self.when;
-                let waker = Arc::new(Mutex::new(cx.waker().clone()));
-                self.waker = Some(waker.clone());
-                thread::spawn(move || {
-                    let now = Instant::now();
-
-                    if now < when {
-                        thread::sleep(when - now);
-                    }
-                    let waker = waker.lock().unwrap();
-                    waker.wake_by_ref();
-                });
-            }
             if Instant::now() >= self.when {
                 Poll::Ready(())
             } else {
+                if !self.has_waker {
+                    let when = self.when;
+
+                    let waker = Arc::new(Mutex::new(cx.waker().clone()));
+
+                    self.has_waker = true;
+
+                    thread::spawn(move || {
+                        let now = Instant::now();
+
+                        if now < when {
+                            thread::sleep(when - now);
+                        }
+
+                        let waker = waker.lock().unwrap();
+
+                        waker.wake_by_ref();
+                    });
+                }
                 Poll::Pending
             }
         }
     }
+
     let future = Delay {
         when: Instant::now() + dur,
-        waker: None,
+
+        has_waker: false,
     };
+
     future.await;
 }
