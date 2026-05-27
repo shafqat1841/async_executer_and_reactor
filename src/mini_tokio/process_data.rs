fde63307615e5cd2ceb4305a0a636ced95c5d9cb
shafqat1841 <shafqat1841@gmail.com>
2026-05-27 18:34:59 +0500

    chore: refactor code

--- src/mini_tokio/process_data.rs
+++ src/mini_tokio/process_data.rs
@@ -4,49 +4,53 @@ use std::task::{Context, Poll};
 use std::thread;
 use std::time::{Duration, Instant};
 
-pub async fn delay(dur: Duration) {
-    struct Delay {
-        when: Instant,
+struct Delay {
+    when: Instant,
 
-        has_waker: bool,
-    }
+    has_waker: bool,
+}
 
-    impl Future for Delay {
-        type Output = ();
+impl Delay {
+    pub fn new(dur: Duration) -> Self {
+        Delay {
+            when: Instant::now() + dur,
 
-        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
-            if Instant::now() >= self.when {
-                Poll::Ready(())
-            } else {
-                if !self.has_waker {
-                    let when = self.when;
-
-                    let waker = Arc::new(Mutex::new(cx.waker().clone()));
-
-                    self.has_waker = true;
-
-                    thread::spawn(move || {
-                        let now = Instant::now();
-
-                        if now < when {
-                            thread::sleep(when - now);
-                        }
-
-                        let waker = waker.lock().unwrap();
-
-                        waker.wake_by_ref();
-                    });
-                }
-                Poll::Pending
-            }
+            has_waker: false,
         }
     }
+}
 
-    let future = Delay {
-        when: Instant::now() + dur,
+impl Future for Delay {
+    type Output = ();
 
-        has_waker: false,
-    };
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        if Instant::now() >= self.when {
+            Poll::Ready(())
+        } else {
+            if !self.has_waker {
+                let when = self.when;
 
-    future.await;
+                let waker = Arc::new(Mutex::new(cx.waker().clone()));
+
+                self.has_waker = true;
+
+                thread::spawn(move || {
+                    let now = Instant::now();
+
+                    if now < when {
+                        thread::sleep(when - now);
+                    }
+
+                    let waker = waker.lock().unwrap();
+
+                    waker.wake_by_ref();
+                });
+            }
+            Poll::Pending
+        }
+    }
+}
+
+pub async fn delay(dur: Duration) {
+    Delay::new(dur).await;
 }
