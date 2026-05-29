f6bca632559671da772acb06b90e4a4638a66853
shafqat1841 <shafqat1841@gmail.com>
2026-05-29 19:14:23 +0500

    chore: refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 mod mini_tokio;
 mod my_runtime;
+
+#[warn(unused_imports)]
 use crate::mini_tokio::main_run;
 use crate::my_runtime::main_run2;
 
 pub fn run() {
     main_run2();
-    main_run();
+    // main_run();
 }
--- src/my_runtime/mod.rs
+++ src/my_runtime/mod.rs
@@ -1,20 +1,3 @@
-mod executor;
-mod mini_runtime;
-mod my_tcp_listener;
-mod process_data_state;
-mod reactor;
-
-use crate::my_runtime::{
-    mini_runtime::{MyRuntime, MyTcpListener},
-    process_data_state::process_data,
-};
-
 pub fn main_run2() {
-    let mut runtime = MyRuntime::new();
-
-    let my_tcp_listener: MyTcpListener = runtime.get_tcp_listener_mut();
-
-    let future = process_data(my_tcp_listener);
-
-    runtime.spawn(future);
+    print!("main_run2");
 }
